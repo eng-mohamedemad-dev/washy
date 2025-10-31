cb5b1f2a9b9bcb14297871feca661175196c9438
eng-mohamedemad-dev <mohamedelghool671@gmail.com>
2025-10-31 09:52:37 +0200

    dev

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wash_flutter/main.dart';
+import 'package:wash_flutter/core/config/locale_notifier.dart';
 
 void main() {
   testWidgets('WashyWash app smoke test', (WidgetTester tester) async {
+    WidgetsFlutterBinding.ensureInitialized();
+    SharedPreferences.setMockInitialValues({});
+    final prefs = await SharedPreferences.getInstance();
+    final localeNotifier = await LocaleNotifier.create(prefs);
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const WashyWashApp());
+    await tester.pumpWidget(WashyWashApp(localeNotifier: localeNotifier));
 
     // Verify that our app starts with loading indicator
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
