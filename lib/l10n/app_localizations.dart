aa6e51cd679009140b0639beac7619ea721c074f
MohamedALAM2 <mtdgdfhjde64@gmail.com>
2025-11-01 11:25:04 +0200

    kosommmek

--- lib/l10n/app_localizations.dart
+++ lib/l10n/app_localizations.dart
@@ -6,11 +6,13 @@ class AppLocalizations {
   final Locale locale;
   AppLocalizations(this.locale);
 
-  static const LocalizationsDelegate<AppLocalizations> delegate = _AppLocalizationsDelegate();
+  static const LocalizationsDelegate<AppLocalizations> delegate =
+      _AppLocalizationsDelegate();
 
   static const List<Locale> supportedLocales = [Locale('ar'), Locale('en')];
 
-  static AppLocalizations of(BuildContext context) => Localizations.of<AppLocalizations>(context, AppLocalizations)!;
+  static AppLocalizations of(BuildContext context) =>
+      Localizations.of<AppLocalizations>(context, AppLocalizations)!;
 
   static final Map<String, Map<String, String>> _localizedValues = {
     'ar': {
@@ -25,6 +27,21 @@ class AppLocalizations {
       'search_hint': 'ابحث عن خدمة الغسيل',
       'categories': 'الفئات',
       'order_now': 'اطلب الآن',
+      // Category translations
+      'clothes': 'الملابس',
+      'furniture': 'الأثاث',
+      'carpet': 'السجاد',
+      'car_detailing': 'تنظيف السيارات',
+      'house_cleaning': 'تنظيف المنازل',
+      'shoes': 'الأحذية',
+      'tailoring': 'الخياطة',
+      'offers': 'العروض',
+      'dresses': 'الفساتين',
+      'bags': 'الحقائب',
+      'wash_and_fold': 'غسيل وطي بالكيلو',
+      'bedding': 'المفروشات',
+      'curtains_cleaning': 'تنظيف الستائر',
+      'ecoclean': 'الإيكو كلين',
     },
     'en': {
       'home': 'Home',
@@ -38,15 +55,33 @@ class AppLocalizations {
       'search_hint': 'Search laundry service',
       'categories': 'Categories',
       'order_now': 'Order Now',
+      // Category translations
+      'clothes': 'Clothes',
+      'furniture': 'Furniture',
+      'carpet': 'Carpet',
+      'car_detailing': 'Car detailing',
+      'house_cleaning': 'House cleaning',
+      'shoes': 'Shoes',
+      'tailoring': 'Tailoring',
+      'offers': 'Offers!',
+      'dresses': 'Dresses',
+      'bags': 'Bags',
+      'wash_and_fold': 'Wash and Fold per kilo',
+      'bedding': 'Bedding',
+      'curtains_cleaning': 'Curtains cleaning',
+      'ecoclean': 'Ecoclean',
     },
   };
 
   String t(String key) {
-    return _localizedValues[locale.languageCode]?[key] ?? _localizedValues['en']![key] ?? key;
+    return _localizedValues[locale.languageCode]?[key] ??
+        _localizedValues['en']![key] ??
+        key;
   }
 }
 
-class _AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
+class _AppLocalizationsDelegate
+    extends LocalizationsDelegate<AppLocalizations> {
   const _AppLocalizationsDelegate();
 
   @override
@@ -58,7 +93,6 @@ class _AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> 
   }
 
   @override
-  bool shouldReload(covariant LocalizationsDelegate<AppLocalizations> old) => false;
+  bool shouldReload(covariant LocalizationsDelegate<AppLocalizations> old) =>
+      false;
 }
-
-
